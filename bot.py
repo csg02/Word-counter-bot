6d9cf58daf6fa5b5709189575d1cbe5b0a446b2b
soulrika <45083953+soulrika@users.noreply.github.com>
2021-04-04 00:17:38 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+import json
+intents = discord.Intents.default()
+intents.guild_messages = True
+bot = commands.Bot(command_prefix='w-', case_insensitive=True, intents=intents, allowed_mentions=discord.AllowedMentions.none())
+configfile = open('config.json', 'r');config = json.load(configfile)
+
+@bot.event
+async def on_ready():
+    print('Word bot is ready.')
+@bot.event
+async def on_message(message):
+    file = open('words.json', 'r');data = json.load(file)
+    await bot.process_commands(message)
+    if message.author.bot:
+        return
+    for word in message.content.split():
+        if not str(message.guild.id) in data:
+            data[str(message.guild.id)] = {}
+        if word not in data[str(message.guild.id)]:
+            data[str(message.guild.id)][word] = 0
+  
+        data[str(message.guild.id)][word] += 1
+   
+        dumps = open("words.json", "w")
+          
+        json.dump(data, dumps, indent = 4)
+        
+
+@bot.command()
+async def word(ctx, word):
+    file = open('words.json', 'r')
+    data = json.load(file)
+    if str(ctx.guild.id)  not in data:
+        return await ctx.send('Guild is not in database, try saying some words!')
+    if word in data[str(ctx.guild.id)]:
+        await ctx.send(f'**{word}** has been said `{data[str(ctx.guild.id)][word]}` time(s)')
+    else:
+        await ctx.send('That word has not been said in this server!')
+
+        
+    
+
+bot.run(config['Token'])
