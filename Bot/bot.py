b7b02678f393973f8aac3737b4550979f546ebf8
soulrika <45083953+soulrika@users.noreply.github.com>
2021-04-04 01:07:58 +0100

    Update bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,11 +1,15 @@
 import discord
 from discord.ext import commands
 import json
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_option
 intents = discord.Intents.default()
 intents.guild_messages = True
+bot = commands.Bot(command_prefix='w-', case_insensitive=True, intents=intents, allowed_mentions=discord.AllowedMentions.none())
+slash = SlashCommand(bot, sync_commands=True)
 
 configfile = open('config.json', 'r');config = json.load(configfile)
-bot = commands.Bot(command_prefix=config['Prefix'], case_insensitive=True, intents=intents, allowed_mentions=discord.AllowedMentions.none())
+
 @bot.event
 async def on_ready():
     print('Word bot is ready.')
@@ -28,11 +32,18 @@ async def on_message(message):
         json.dump(data, dumps, indent = 4)
         
 
-@bot.command()
-async def word(ctx, word):
+@slash.slash(name='word', description='See how many times a word has been said in this server',options=[
+               create_option(
+                 name="word",
+                 description="The word to get the word count for",
+                 option_type=3,
+                 required=True
+               )
+             ])
+async def word(ctx, word: str):
     file = open('words.json', 'r')
     data = json.load(file)
-    if str(ctx.guild.id)  not in data:
+    if str(ctx.guild.id) not in data:
         return await ctx.send('Guild is not in database, try saying some words!')
     if word in data[str(ctx.guild.id)]:
         await ctx.send(f'**{word}** has been said `{data[str(ctx.guild.id)][word]}` time(s)')
